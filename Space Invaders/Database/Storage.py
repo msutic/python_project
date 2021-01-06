759e6089faac1d0c23495ce79b428bcad54b73d9
Marko Sutic <msuticm@gmail.com>
2021-01-06 18:51:15 +0100

    1

--- Space Invaders/Database/Storage.py
+++ Space Invaders/Database/Storage.py
@@ -18,25 +18,25 @@ class Storage:
     
     def get_all_players(self):
         return self.players
-
+    
     def get_all_shields(self):
         return self.shields
-
+    
     def get_all_spaceships(self):
         return self.spaceships
-
+    
     # APPEND FUNCTIONS
     def add_bullet(self, bullet: Bullet):
         self.bullets.append(bullet)
-
+        
     def add_alien(self, alien: Alien):
         self.aliens.append(alien)
 
     def add_player(self, player: Player):
         self.players.append(player)
-
+        
     def add_shield(self, shield: Shield):
         self.shields.append(shield)
-
+        
     def add_spaceship(self, spaceship: Spaceship):
         self.spaceships.append(spaceship)
