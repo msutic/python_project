79a3b1bdc3571924d1f25c8717100e8ddae07edb
Marko Sutic <msuticm@gmail.com>
2021-01-06 18:12:40 +0100

    updated storage class

--- Space Invaders/Database/Storage.py
+++ Space Invaders/Database/Storage.py
@@ -1,13 +1,42 @@
-from Entities import Bullet
+from Entities import Bullet, Alien, Player, Shield, Spaceship
 
 
 class Storage:
-    def __init__(self, bullets=()):
+    def __init__(self, bullets=(), aliens=(), players=(), shields=(), spaceships=()):
         self.bullets = list(bullets)
+        self.aliens = list(aliens)
+        self.players = list(players)
+        self.shields = list(shields)
+        self.spaceships = list(spaceships)
 
-    def get_bullets(self):
+    # GETTERS
+    def get_all_bullets(self):
         return self.bullets
 
+    def get_all_aliens(self):
+        return self.aliens
+    
+    def get_all_players(self):
+        return self.players
+
+    def get_all_shields(self):
+        return self.shields
+
+    def get_all_spaceships(self):
+        return self.spaceships
+
+    # APPEND FUNCTIONS
     def add_bullet(self, bullet: Bullet):
         self.bullets.append(bullet)
 
+    def add_alien(self, alien: Alien):
+        self.aliens.append(alien)
+
+    def add_player(self, player: Player):
+        self.players.append(player)
+
+    def add_shield(self, shield: Shield):
+        self.shields.append(shield)
+
+    def add_spaceship(self, spaceship: Spaceship):
+        self.spaceships.append(spaceship)
